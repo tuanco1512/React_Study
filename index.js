0454dbec66849b9a1092931143b34814f772144f
tuanco1512 <tuanco15122000@gmail.com>
2023-12-28 12:18:08 +0700

    a

---
+++ index.js
@@ -0,0 +1,122 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+// function Navbar() {
+//     return (
+//         <nav className="navbar navbar-expand-lg navbar-light bg-light">
+//             <a className="navbar-brand" href="#">Navbar</a>
+//             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+//                 <span className="navbar-toggler-icon"></span>
+//             </button>
+
+//             <div className="collapse navbar-collapse" id="navbarSupportedContent">
+//                 <ul className="navbar-nav mr-auto">
+//                     <li className="nav-item active">
+//                         <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+//                     </li>
+//                     <li className="nav-item">
+//                         <a className="nav-link" href="#">Link</a>
+//                     </li>
+//                     <li className="nav-item dropdown">
+//                         <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+//                             Dropdown
+//                         </a>
+//                         <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+//                             <a className="dropdown-item" href="#">Action</a>
+//                             <a className="dropdown-item" href="#">Another action</a>
+//                             <div className="dropdown-divider"></div>
+//                             <a className="dropdown-item" href="#">Something else here</a>
+//                         </div>
+//                     </li>
+//                     <li className="nav-item">
+//                         <a className="nav-link disabled" href="#">Disabled</a>
+//                     </li>
+//                 </ul>
+//                 <form className="form-inline my-2 my-lg-0">
+//                     <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+//                     <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+//                 </form>
+//             </div>
+//         </nav>
+//     )
+// }
+
+// function MainContent() {
+//     return (
+//         <h1>I'm learning React!</h1>
+//     )
+// }
+
+// // Challenge: Create your own custom React component!
+// // Call it "MainContent", and have it return a simple
+// // h1 element that says "I'm learning React!"
+
+// // Afterward, render it below the Navbar (which
+// // you can do inside the ReactDOM.render call below)
+
+// ReactDOM.render(
+//     <div>
+//         <Navbar />
+//         <MainContent />
+//     </div>,
+//     document.getElementById("root")
+// )
+
+
+
+// ReactDOM.render(<h1 className="header">Hello, React!</h1>, document.getElementById("root")) 
+
+/* 
+Challenge - recreate the above line of code in vanilla JS by creating and
+appending an h1 to our div#root (without using innerHTML).
+
+- Create a new h1 element
+- Give it some textContent
+- Give it a class name of "header"
+- append it as a child of the div#root
+    
+*/
+
+// const h1 = document.createElement("h1")
+// h1.textContent = "This is an imperative way to program"
+// h1.className = "header"
+// document.getElementById("root").append(h1)
+
+
+
+// const page = (
+//     <div>
+//         <h1 className="header">This is JSX</h1>
+//         <p>This is a paragraph</p>
+//     </div>
+// )
+
+/* 
+Challenge: 
+
+Create a navbar in JSX:
+    - Use the semantic `nav` element as the parent wrapper
+    - Have an h1 element with the brand name of your "website"
+    - Insert an unordered list for the other nav elements
+        - Inside the `ul`, have three `li`s for "Pricing",
+        "About", and "Contact"
+    - Don't worry about styling yet - it'll just be plain-looking HTML for now
+*/
+
+const navbar = (
+    <nav>
+        <h1>My Website</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+// ReactDOM.render(
+//     navbar,
+//     document.getElementById("root")
+// )
+
+ReactDOM.createRoot(document.getElementById("Root")).render(navbar);
